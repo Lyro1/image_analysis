12b90281ff177c1569f6344842b6e426970f98b0
d594416 <d594416@SafranTest.home>
2018-10-03 10:42:45 +0200

    Added a new version of the install_plaso script

--- install_plaso.sh
+++ install_plaso.sh
@@ -1,58 +1,21 @@
 #!/bin/sh
 
-force=0
-
-if [ "$#" -gt 1 ]; then
-	echo "Usage: ./install_plaso.sh <option>"
-	exit 1
-fi
-
-if [ "$#" -eq 1 ]; then
-	if [ "$1" -eq "-f" ]; then
-		force=1
-	fi 
-fi
-
-echo "Downloading and installing all the dependencies."
-sudo apt-get install build-essential autotools-dev libsqlite3-dev python-dev debhelper devscripts fakeroot quilt git mercurial python-setuptools libtool automake -y
-
+echo "Getting the l2tdevtools to install Plaso."
+sudo rm -rR l2tdevtools
+git clone https://github.com/log2timeline/l2tdevtools.git
 if [ "$?" -ne 0 ]; then
-	echo "Error: failed to get all the dependencies."
-	exit 4
+    echo "Error: failed to clone the l2tdevtools repository."
+    exit 1
 fi
+echo "Successfully cloned the l2tdevtools repository."
 
-if [ ! -f "plaso-20180818.tar.gz" || "$force" -eq 1 ]; then
-	wget "https://github.com/log2timeline/plaso/releases/download/20180818/plaso-20180818.tar.gz"
-	if [ "$?" -ne 0 ]; then
-		echo "Error: failed to download the plaso package."
-		exit 5
-	fi
+cd l2tdevtools
 
-	echo "Sucesfully downloaded the plaso package."
-else
-	echo "Plaso package already downloaded"
-fi
+echo "Getting the pre-requisites dependencies."
+sudo apt-get install build-essential autoconf automake autopoint libtool gettext pkg-config debhelper devscripts fakeroot quilt autotools-dev zlib1g-dev libbz2-dev libssl-dev libfuse-dev libfuse-dev python-dev python-setuptools flex byacc python3-all python3-setuptools python3-all-dev liblzma-dev python-pbr python3-pbr python-setuptools-scm python3-setuptools-scm -y
 
-tar zxvf plaso-20180818.tar.gz
-if [ "$?" -ne 0 ]; then
-	echo "Failed to extract the plaso package."
-	exit 6
-fi
+echo "Building target dpkg."
+PYTHONPATH=. python tools/build.py --preset plaso dpkg
 
-echo "Sucesfully extracted the plaso package."
-
-cp -rf plaso-20180818/config/dpkg plaso-20180818/debian
-
-if [ "$?" -ne 0 ]; then
-	echo "Failed to move the config/dpkg folder to debian folder."
-	exit 7
-fi
-
-echo "Sucesfully moved the config/dpkg folder to debian"
-
-PYTHONPATH=l2tdevtools l2tdevtools/tools/dpkg-generate.py --source-directory=. plaso-20180818.tar.gz
-mv dpkg debian
-
-// La ligne au dessus ne fonctionne pas encore (cf mail au développeur). 
-
-exit 0
+cd build
+echo "Built all the packages."
