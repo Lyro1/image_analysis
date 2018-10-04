b227192f118cd9ce53b04f88ff17794304309251
Lyro1 <liro1du78@live.fr>
2018-10-04 12:56:00 +0200

    Installation and start of ElasticSearch
    The script now manages when ElasticSearch is not installed and starts it when
    needed. If ElasticSearch is not installed, it asks for an installation.
    If ElasticSearch is not started, it starts the program without asking.

--- match_hash.sh
+++ match_hash.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+install_elasticsearch() {
+    wget "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.4.2.deb" -q --show-progress
+    sudo dpkg -i "elasticsearch-6.4.2.deb"
+    sudo apt-get install -f
+    sudo rm -rRf "elasticsearch-6.4.2.deb"
+}
+
 if [ "$#" -ne 6 ]; then
     echo "[ERROR] Usage: ./match_hash -m <mode> -f <file to filter> -c <source>"
     exit 1
@@ -35,14 +42,39 @@ if [ ! -f "$6" ]; then
     exit 7
 fi
 
-# TODO: Verifier que ElasticSearch est installé
-# Si non : installer
-# Si oui : Verifier que ElasticSearch est allumé
-#          Si non : allumer
-#          Si oui : checker le mode
-#                   Si le mode est 'g' : supprimer du csv toutes les entrées
-#                                        qui matchent la source
-#                   Si le mode est 'b' : ne guarder que les entrées qui 
-#                                        matchent la source
-#
+echo "[INFO] Verify ElasticSearch installation."
+status=$(dpkg -s elasticsearch | grep Status)
+deinstall=$(echo "$status" | grep "deinstall")
+if [ "$?" -ne 0 ] || [ ! -z "$deinstall" ]; then
+    echo "[ERROR] ElasticSearch is not installed."
+    echo "[INFO] Start the installation? (y/n)"
+    read answer
+    if [ "$answer" != "y" ] && [ "$answer" != "n" ]; then
+        echo "[ERROR] Invalid answer. Just type 'y' (yes) or 'n' (no)"
+        exit 8
+    fi
+    if [ "$answer" = "y" ]; then
+        install_elasticsearch
+    else
+        echo "[INFO] Not installing ElasticSearch."
+        exit 9
+    fi
+fi
+
+echo "[INFO] ElasticSearch installed."
+
+ps ax | grep -v grep | grep "elasticsearch" > /dev/null
+if [ "$?" -eq 1 ]; then
+    echo "[INFO] ElasticSearch not running. Starting."
+    if [ ! -z "$(ps -p 1 | grep "systemd")" ]; then
+        echo "[INFO] System running systemd."
+        sudo systemctl start elasticsearch.service
+    else
+        echo "[INFO] System running init."
+        sudo -i service elastisearch start
+    fi
+fi 
+
+echo "[INFO] ElasticSearch started."
+
 exit 0
