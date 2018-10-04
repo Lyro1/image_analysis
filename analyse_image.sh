383290432f2d7d3d201a33066c066af93985f234
Lyro1 <liro1du78@live.fr>
2018-10-04 16:10:30 +0200

    Generation of .plaso and .log files
    Should work, just got a wierd issue with artifacts (#2168 on plas issues on Github)

--- analyse_image.sh
+++ analyse_image.sh
@@ -13,3 +13,25 @@ if [ -z $(find . -maxdepth 1 -type d -name 'plaso-*' -print -quit) ]; then
 fi
 
 echo "[INFO] Plaso is installed."
+
+echo "[INFO] Generating the Plaso file for the provided image"
+log2timeline.py "outputs/$1-result.plaso" "$1"
+
+if [ "$?" -ne 0 ]; then
+    echo "[ERROR] Failed to generate Plaso file."
+    exit 3
+fi
+
+echo "[INFO] Generated outputs/$1-result.plaso."
+echo "[INFO] Analysing the result file."
+
+psort.py -w "outputs/$1-result.log" "outputs/$1-result.plaso"
+
+if [ "$?" -ne 0 ]; then
+    echo "[ERROR] Failed to analyse the result file."
+    exit 4
+fi
+
+echo "[INFO] Generated outputs/$1-result.log with the analyse results"
+
+exit 0
