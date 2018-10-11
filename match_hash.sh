46b0a675d99e1e1088b36e2eadb8acece7f19976
Lyro1 <liro1du78@live.fr>
2018-10-11 16:56:30 +0200

    Added the name of unknown file

--- match_hash.sh
+++ match_hash.sh
@@ -73,6 +73,8 @@ hashfile="$2"
 if [ "$1" = "-d" ]; then
     sudo md5deep -r "$2" > hashfile.txt
     hashfile="hashfile.txt"
+    numberoffiles=$(wc -l "$hashfile" | awk '{ print $1 }')
+    echo "[INFO] Found $numberoffiles files in $2"
 fi
 name="$2"
 lastchar=$(echo -n "$name" | tail -c 1)
@@ -85,17 +87,29 @@ date=$(date "+%d%m%y-%H%M%S")
 sudo mkdir "hash_outputs" -p
 
 res=$(nsrllookup < "$hashfile")
-resfile="hash_outputs/$resname-$date.txt"
+resfile="hash_outputs/$resname-$date-1.txt"
+resfinal="hash_outputs/$resname-$date.txt"
 sudo touch "$resfile"
+sudo touch "$resfinal"
 sudo chmod 666 "$resfile"
-echo "[INFO] Created $resfile"
+sudo chmod 666 "$resfinal"
+echo "[INFO] Created $resfinal"
 sudo echo "$res" >> "$resfile"
 sudo chmod 644 "$resfile"
-echo "[INFO] Saved the result in $resfile"
+echo "[INFO] Saved the result in $resfinal"
+
+echo "[INFO] Getting files names."
+while IFS='' read -r line; do
+    name=$(grep -i "$line" "$hashfile")
+    if [ ! -z "$name" ]; then
+        echo "$name" >> "$resfinal"
+    fi
+done < "$resfile"
+sudo chmod 644 "$resfinal"
 
 echo "[INFO] Remove trash files."
-sudo rm -rRf hashfile.txt set
-unknownfiles=$(wc -l "$resfile" | awk '{ print $1 }')
+sudo rm -rRf set "hashfile.txt" "$resfile"
+unknownfiles=$(wc -l "$resfinal" | awk '{ print $1 }')
 echo "[INFO] Analyse complete."
 echo "[INFO] $unknownfiles unknown files were found."
 
